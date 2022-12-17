af939b982636814eceef082aecc244da9beaec85
Robert Pack <robstar.pack@gmail.com>
2022-12-17 14:42:32 +0100

    test: add conversion tests

---
+++ inference-protocol/README.md
@@ -0,0 +1,3 @@
+# inference-protocol
+
+Generated types for the [V2 inference API](https://kserve.github.io/website/modelserving/inference_api/#grpc)
--- inference-protocol/src/tract.rs
+++ inference-protocol/src/tract.rs
@@ -56,3 +56,60 @@ impl TryFrom<ModelInferRequest> for TVec<Tensor> {
             .collect::<TractResult<TVec<Tensor>>>()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tensor_conversion() {
+        let values: Vec<f32> = vec![1.1, 1.2, 1.3, 2.1, 2.2, 2.3];
+        let input = InferInputTensor {
+            name: "tensor".into(),
+            datatype: "fp32".into(),
+            shape: vec![3, 2],
+            parameters: Default::default(),
+            contents: Some(InferTensorContents {
+                fp32_contents: values.clone(),
+                ..Default::default()
+            }),
+        };
+        let reference = tract_ndarray::arr1(&values)
+            .into_shape((3, 2))
+            .unwrap()
+            .into_tensor();
+        let tensor: Tensor = input.try_into().unwrap();
+
+        assert_eq!(reference, tensor)
+    }
+
+    #[test]
+    fn test_infer_request_conversion() {
+        let values: Vec<f32> = vec![1.1, 1.2, 1.3, 2.1, 2.2, 2.3];
+        let input = InferInputTensor {
+            name: "tensor".into(),
+            datatype: "fp32".into(),
+            shape: vec![3, 2],
+            parameters: Default::default(),
+            contents: Some(InferTensorContents {
+                fp32_contents: values.clone(),
+                ..Default::default()
+            }),
+        };
+        let request = ModelInferRequest {
+            model_name: "model".into(),
+            model_version: "v1".into(),
+            inputs: vec![input],
+            ..Default::default()
+        };
+
+        let reference: TVec<Tensor> = vec![tract_ndarray::arr1(&values)
+            .into_shape((3, 2))
+            .unwrap()
+            .into_tensor()]
+        .into();
+        let tvec: TVec<Tensor> = request.try_into().unwrap();
+
+        assert_eq!(reference, tvec)
+    }
+}
